ca06d0ed46322e3e8c427e0e527c7b7613381fd0
jordan-wood02 <jcw43@email.sc.edu>
2024-01-23 17:17:06 -0500

    Minor changes

--- fullstack-ai-chatbot/server/main.py
+++ fullstack-ai-chatbot/server/main.py
@@ -1,20 +1,23 @@
-from fastapi import FASTAPI, Request
+from fastapi import FastAPI, Request
 import uvicorn
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-api = FASTAPI()
+api = FastAPI()
+
+# Simple test route to test the API
 
 
 @api.get("/test")
 async def root():
     return {"msg": "API is Online"}
 
-
+# Set up development server
+# API will run on port 3500
 if __name__ == "__main__":
-    if os.environ.get('APP_UNV') == "development":
+    if os.environ.get('APP_ENV') == "development":
         uvicorn.run("main:api", host="0.0.0.0", port=3500,
                     workers=4, reload=True)
     else:
